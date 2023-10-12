9ceb2b19009ce0e22ee5aae832f2b332198c1517
NURSENA BİTİRGEN <94117345+senabibi@users.noreply.github.com>
2023-10-12 05:42:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
-"""student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-"""
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-import pandas
-data=pandas.read_csv(r"C:\Users\SENA\Downloads\NATO-alphabet-start\NATO-alphabet-start\nato_phonetic_alphabet.csv")
+#student_dict = {
+#    "student": ["Angela", "James", "Lily"], 
+#    "score": [56, 76, 98]
+#}
+#
+##Looping through dictionaries:
+#for (key, value) in student_dict.items():
+#    #Access key and value
+#    pass
+#
+#import pandas
+#student_data_frame = pandas.DataFrame(student_dict)
+#
+##Loop through rows of a data frame
+#for (index, row) in student_data_frame.iterrows():
+#    #Access index and row
+#    #Access row.student or row.score
+#    pass
+#
+## Keyword Method with iterrows()
+## {new_key:new_value for (index, row) in df.iterrows()}
+#
+#TODO 1. Create a dictionary in this format:
+#{"A": "Alfa", "B": "Bravo"}
+import pandas 
+data=pandas.read_csv("nato_phonetic_alphabet.csv")
 print(data.to_dict())
-new_dict={row.letter:row.code for (index, row) in data.iterrows()}
-print(new_dict)
-
-word=input("Enter the word:").upper()
-
-last_dict=[new_dict[letter] for letter in word]
-print(last_dict)
-
-
+phonetic_dict={row.letter:row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word=input("Enter a word:").upper()
+output_list=[phonetic_dict[letter] for letter in word]
+print(output_list)
